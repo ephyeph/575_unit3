6b537f6efefb4b0275841b2fe4ddcb3a923fbaf2
ephyeph <ephra03@gmail.com>
2025-04-03 20:45:59 -0400

    lesson3

--- js/main.js
+++ js/main.js
@@ -1,11 +1,9 @@
-// Execute script when window is loaded
 window.onload = function() {
 
-    // SVG dimension variables
-    var w = 900,
-        h = 500;
+    // Dimensions
+    var w = 900, h = 500;
 
-    // SVG container block
+    // SVG container
     var container = d3.select("body")
         .append("svg")
         .attr("width", w)
@@ -13,18 +11,18 @@ window.onload = function() {
         .attr("class", "container")
         .style("background-color", "rgba(0,0,0,0.2)");
 
-    // Inner white rectangle block
+    // White inner rect
     var innerRect = container.append("rect")
         .datum(400)
-        .attr("width", function(d) { return d * 2; }) // 800
-        .attr("height", function(d) { return d; })    // 400
+        .attr("width", function(d) { return d * 2; })
+        .attr("height", function(d) { return d; })
         .attr("class", "innerRect")
         .attr("x", 50)
         .attr("y", 50)
         .style("fill", "#FFFFFF")
         .style("stroke", "none");
 
-    // City population array
+    // Data array
     var cityPop = [
         { city: 'Madison', population: 233209 },
         { city: 'Milwaukee', population: 594833 },
@@ -32,26 +30,71 @@ window.onload = function() {
         { city: 'Superior', population: 27244 }
     ];
 
-    // Circle block – data join
+    // Min/max calculations
+    var minPop = d3.min(cityPop, d => d.population);
+    var maxPop = d3.max(cityPop, d => d.population);
+
+    // Scales
+    var x = d3.scaleLinear()
+        .range([90, 840])  // Increased max to fit labels
+        .domain([0, cityPop.length - 1]);
+
+    var y = d3.scaleLinear()
+        .range([450, 50])
+        .domain([0, 700000]);
+
+    var color = d3.scaleLinear()
+        .range(["#FDBE85", "#D94701"])
+        .domain([minPop, maxPop]);
+
+    // Axis
+    var yAxis = d3.axisLeft(y);
+
+    var axis = container.append("g")
+        .attr("class", "axis")
+        .attr("transform", "translate(50, 0)")
+        .call(yAxis);
+
+    // Title
+    var title = container.append("text")
+        .attr("class", "title")
+        .attr("text-anchor", "middle")
+        .attr("x", 450)
+        .attr("y", 30)
+        .text("City Populations");
+
+    // Circles
     var circles = container.selectAll(".circles")
         .data(cityPop)
         .enter()
         .append("circle")
         .attr("class", "circles")
-        .attr("id", function(d) {
-            return d.city;
-        })
-        .attr("r", function(d) {
-            // Calculate radius based on population area
-            var area = d.population * 0.01;
-            return Math.sqrt(area / Math.PI);
-        })
-        .attr("cx", function(d, i) {
-            // Space out circles horizontally using index
-            return 90 + (i * 180);
-        })
-        .attr("cy", function(d) {
-            // Grow circles upward by subtracting from 450
-            return 450 - (d.population * 0.0005);
-        });
+        .attr("id", d => d.city)
+        .attr("r", d => Math.sqrt((d.population * 0.01) / Math.PI))
+        .attr("cx", (d, i) => x(i))
+        .attr("cy", d => y(d.population))
+        .style("fill", d => color(d.population))
+        .style("stroke", "#000");
+
+    // Labels (2-line with tspan)
+    var format = d3.format(",");
+
+    var labels = container.selectAll(".labels")
+        .data(cityPop)
+        .enter()
+        .append("text")
+        .attr("class", "labels")
+        .attr("text-anchor", "left")
+        .attr("y", d => y(d.population) - 5); // slightly above vertical center
+
+    var nameLine = labels.append("tspan")
+        .attr("class", "nameLine")
+        .attr("x", (d, i) => x(i) + Math.sqrt((d.population * 0.01) / Math.PI) + 5)
+        .text(d => d.city);
+
+    var popLine = labels.append("tspan")
+        .attr("class", "popLine")
+        .attr("x", (d, i) => x(i) + Math.sqrt((d.population * 0.01) / Math.PI) + 5)
+        .attr("dy", "15")
+        .text(d => "Pop. " + format(d.population));
 };
