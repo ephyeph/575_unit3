e1ab8cd9dc77ef94d07e907c75c2025fa476a2bf
ephyeph <ephra03@gmail.com>
2025-04-03 20:18:09 -0400

    1st

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
-// Add all scripts to the JS folder
+//execute script when window is loaded
+window.onload = function() {
+
+    //SVG dimension variables
+    var w = 900,
+        h = 500;
+
+    //container block
+    var container = d3.select("body") //select the body element
+        .append("svg") //append an svg element
+        .attr("width", w) //set width
+        .attr("height", h) //set height
+        .attr("class", "container") //set class name
+        .style("background-color", "rgba(0,0,0,0.2)"); //light transparent background
+
+    //inner rectangle block
+    var innerRect = container.append("rect") //append rectangle to svg
+        .datum(400) //bind datum to it
+        .attr("width", function(d) {
+            return d * 2; //width: 800
+        })
+        .attr("height", function(d) {
+            return d; //height: 400
+        })
+        .attr("class", "innerRect") //assign class
+        .attr("x", 50) //position from left
+        .attr("y", 50) //position from top
+        .style("fill", "#FFFFFF"); //fill color
+};
