c2dcee73cc893aaf8a1a8def508d97fb04e171fd
ephyeph <ephra03@gmail.com>
2025-04-06 18:35:33 -0400

    start10

---
+++ js/activity9.js
@@ -0,0 +1,70 @@
+window.onload = setMap;
+
+function setMap() {
+    var width = 960,
+        height = 460;
+
+    // Create SVG container for the map
+    var map = d3.select("body")
+        .append("svg")
+        .attr("class", "map")
+        .attr("width", width)
+        .attr("height", height);
+
+    // Create an Albers Equal-Area projection centered over Germany
+    var projection = d3.geoAlbers()
+        .center([10.5, 51]) // roughly center of Germany
+        .rotate([-10.5, 0])
+        .parallels([40, 60])
+        .scale(3000)
+        .translate([width / 2 + 150, height / 2]);
+
+    // Create path generator
+    var path = d3.geoPath()
+        .projection(projection);
+
+    // Load data
+    var promises = [
+        d3.csv("data/unitsData.csv"),
+        d3.json("data/de.topojson")
+    ];
+    
+
+    Promise.all(promises).then(callback);
+
+    function callback(data) {
+        var csvData = data[0],
+            germany = data[1];
+
+        // Convert TopoJSON to GeoJSON
+        var geojsonData = topojson.feature(germany, germany.objects.de).features;
+
+        // --- Graticule generator ---
+        var graticule = d3.geoGraticule().step([5, 5]);
+
+        // Draw graticule background
+        var gratBackground = map.append("path")
+            .datum(graticule.outline())
+            .attr("class", "gratBackground")
+            .attr("d", path);
+
+        // Draw graticule lines
+        var gratLines = map.selectAll(".gratLines")
+            .data(graticule.lines())
+            .enter()
+            .append("path")
+            .attr("class", "gratLines")
+            .attr("d", path);
+
+
+        // Draw German states
+        map.selectAll(".state")
+            .data(geojsonData)
+            .enter()
+            .append("path")
+            .attr("class", function(d) {
+                return "state " + d.properties.adm1_code;
+            })
+            .attr("d", path);
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,70 +1,131 @@
-window.onload = setMap;
+(function() {
 
-function setMap() {
-    var width = 960,
-        height = 460;
+    // Pseudo-global variables
+    var attrArray = ["varA", "varB", "varC", "varD", "varE"];
+    var expressed = attrArray[0];
 
-    // Create SVG container for the map
-    var map = d3.select("body")
-        .append("svg")
-        .attr("class", "map")
-        .attr("width", width)
-        .attr("height", height);
+    window.onload = setMap;
 
-    // Create an Albers Equal-Area projection centered over Germany
-    var projection = d3.geoAlbers()
-        .center([10.5, 51]) // roughly center of Germany
-        .rotate([-10.5, 0])
-        .parallels([40, 60])
-        .scale(3000)
-        .translate([width / 2 + 150, height / 2]);
+    function setMap() {
+        var width = 960,
+            height = 460;
 
-    // Create path generator
-    var path = d3.geoPath()
-        .projection(projection);
+        // Create SVG container for the map
+        var map = d3.select("body")
+            .append("svg")
+            .attr("class", "map")
+            .attr("width", width)
+            .attr("height", height);
 
-    // Load data
-    var promises = [
-        d3.csv("data/unitsData.csv"),
-        d3.json("data/de.topojson")
-    ];
-    
+        // Albers projection centered over Germany
+        var projection = d3.geoAlbers()
+            .center([10.5, 51])
+            .rotate([-10.5, 0])
+            .parallels([40, 60])
+            .scale(3000)
+            .translate([width / 2 + 150, height / 2]);
 
-    Promise.all(promises).then(callback);
+        var path = d3.geoPath().projection(projection);
 
-    function callback(data) {
-        var csvData = data[0],
-            germany = data[1];
+        var promises = [
+            d3.csv("data/unitsData.csv"),
+            d3.json("data/de.topojson")
+        ];
 
-        // Convert TopoJSON to GeoJSON
-        var geojsonData = topojson.feature(germany, germany.objects.de).features;
+        Promise.all(promises).then(callback);
 
-        // --- Graticule generator ---
+        function callback(data) {
+            var csvData = data[0],
+                germany = data[1];
+
+            var geojsonData = topojson.feature(germany, germany.objects.de).features;
+
+            // Create graticule
+            setGraticule(map, path);
+
+            // Join CSV to GeoJSON
+            geojsonData = joinData(geojsonData, csvData);
+
+            // Create color scale
+            var colorScale = makeColorScale(csvData);
+
+            // Add enumeration units to map
+            setEnumerationUnits(geojsonData, map, path, colorScale);
+        }
+    }
+
+    function setGraticule(map, path) {
         var graticule = d3.geoGraticule().step([5, 5]);
 
-        // Draw graticule background
-        var gratBackground = map.append("path")
+        map.append("path")
             .datum(graticule.outline())
             .attr("class", "gratBackground")
             .attr("d", path);
 
-        // Draw graticule lines
-        var gratLines = map.selectAll(".gratLines")
+        map.selectAll(".gratLines")
             .data(graticule.lines())
             .enter()
             .append("path")
             .attr("class", "gratLines")
             .attr("d", path);
+    }
 
+    function joinData(geojsonData, csvData) {
+        for (var i = 0; i < csvData.length; i++) {
+            var csvRegion = csvData[i];
+            var csvKey = csvRegion.adm1_code;
 
-        // Draw German states
-        map.selectAll(".state")
+            for (var a = 0; a < geojsonData.length; a++) {
+                var geoProps = geojsonData[a].properties;
+                var geoKey = geoProps.adm1_code;
+
+                if (geoKey === csvKey) {
+                    attrArray.forEach(function(attr) {
+                        var val = parseFloat(csvRegion[attr]);
+                        geoProps[attr] = val;
+                    });
+                }
+            }
+        }
+        return geojsonData;
+    }
+
+    function makeColorScale(data) {
+        var colorClasses = [
+            "#D4B9DA",
+            "#C994C7",
+            "#DF65B0",
+            "#DD1C77",
+            "#980043"
+        ];
+
+        var colorScale = d3.scaleQuantile().range(colorClasses);
+
+        var domainArray = [];
+        for (var i = 0; i < data.length; i++) {
+            var val = parseFloat(data[i][expressed]);
+            if (!isNaN(val)) {
+                domainArray.push(val);
+            }
+        }
+
+        colorScale.domain(domainArray);
+        return colorScale;
+    }
+
+    function setEnumerationUnits(geojsonData, map, path, colorScale) {
+        map.selectAll(".regions")
             .data(geojsonData)
             .enter()
             .append("path")
             .attr("class", function(d) {
-                return "state " + d.properties.adm1_code;
+                return "regions " + d.properties.adm1_code;
             })
-            .attr("d", path);
+            .attr("d", path)
+            .style("fill", function(d) {
+                var value = d.properties[expressed];
+                return value ? colorScale(value) : "#ccc";
+            });
     }
-}
+
+})();
